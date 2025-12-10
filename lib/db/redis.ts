6cfd4b0c15be8d749bf9abed7b6f5fde05a1b56c
pouria drd <pouriadrd@gmail.com>
2025-12-10 23:09:42 +0330

    added ketprefix for redis

--- lib/db/redis.ts
+++ lib/db/redis.ts
@@ -1,5 +1,8 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379", {
+	name: process.env.REDIS_APP_NAME || "fogx",
+	keyPrefix: (process.env.REDIS_APP_NAME || "fogx") + ":",
+});
 
 export default redis;
