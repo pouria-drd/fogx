4ffc0a50ff7e4acec71573e2816b0aa7b5a154a7
pouria drd <pouriadrd@gmail.com>
2025-12-10 17:33:08 +0330

    init redis

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -1,35 +1,44 @@
+import { redis } from "@/lib/db";
+import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
 	const body = await req.json();
+
+	const roomId = nanoid();
 	const { username } = body;
 
-	if (!username || username.length < 3) {
-		return NextResponse.json(
-			{
-				success: false,
-				message: "Validation error",
-				statusCode: 400,
-				errors: {
-					username: [
-						{
-							message: "Username must be at least 3 characters",
-							code: "USERNAME_TOO_SHORT",
-						},
-					],
-				},
-			},
-			{ status: 400 },
-		);
-	}
+	await redis.hset(`meta:${roomId}`, {
+		connected: [],
+		createdAt: Date.now(),
+	});
 
 	return NextResponse.json({
 		success: true,
 		message: "Room created",
 		statusCode: 201,
 		result: {
-			id: "abc123",
+			roomId,
 			username,
 		},
 	});
 }
+
+// if (!username || username.length < 3) {
+// 	return NextResponse.json(
+// 		{
+// 			success: false,
+// 			message: "Validation error",
+// 			statusCode: 400,
+// 			errors: {
+// 				username: [
+// 					{
+// 						message: "Username must be at least 3 characters",
+// 						code: "USERNAME_TOO_SHORT",
+// 					},
+// 				],
+// 			},
+// 		},
+// 		{ status: 400 },
+// 	);
+// }
